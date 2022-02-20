c862a9c61a43a0c8ab256735e1ed1593bc58694e
JanPat <46287720+JanPat@users.noreply.github.com>
2022-02-20 15:04:10 +0000

    Add files via upload

---
+++ GPS.py
@@ -0,0 +1,19 @@
+import serial
+import time
+import string
+import pynmea2
+
+#GPS gets coordinates
+#UART communication protocols
+def getCoordinates():
+	port="/dev/ttyAMA0"
+	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+	dataout = pynmea2.NMEAStreamReader()
+	newdata=ser.readline()
+
+	if newdata[0:6] == "$GPRMC":
+		newmsg=pynmea2.parse(newdata)
+		lat=newmsg.latitude
+		lng=newmsg.longitude
+		gps = [lat,lng] 
+		return gps
---
+++ display.py
@@ -0,0 +1,26 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import ST7735
+import time
+
+#OLED display to host the user interface
+#inputs an array to display on the screen
+class OLED:
+	def display(text):
+		disp = ST7735.ST7735(port=0, cs=0, dc=24, backlight=None, rst=25, width=128, height=160, rotation=0, invert=False) 
+		WIDTH = 128
+		HEIGHT = 128
+		offset = 5
+		img = Image.new('RGB', (WIDTH, HEIGHT))
+		draw = ImageDraw.Draw(img)
+
+		# Load default font.
+		font = ImageFont.load_default()
+
+		for element in text:
+			draw.text((5, offset),str(element) , font=font, fill=(255, 255, 255))
+			offset+=10
+		
+		disp.display(img) 
